a1772f7d4b4c3dd24d5bc815b7149853b18b6a24
xushuai <xushvai@gmail.com>
2014-11-16 23:43:41 +0800

    add some test code
    Signed-off-by: xushuai <xushvai@gmail.com>

--- framework/netio/main.cpp
+++ framework/netio/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include "../../include/os.h"
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     // insert code here...
+    std::clog << sizeof(int8) << std::endl;
+    std::clog << sizeof(int16) << std::endl;
+    std::clog << sizeof(int32) << std::endl;
+    std::clog << sizeof(uint8) << std::endl;
+    std::clog << sizeof(uint16) << std::endl;
+    std::clog << sizeof(uint32) << std::endl;
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
--- include/os.h
+++ include/os.h
@@ -1,6 +1,14 @@
 #ifndef __OS_H__
 #define __OS_H__
 
+#include <cstdint>
+
+typedef std::int8_t     int8;
+typedef std::int16_t	int16;
+typedef std::int32_t	int32;
+typedef std::uint8_t	uint8;
+typedef std::uint16_t	uint16;
+typedef std::uint32_t	uint32;
 
 #ifdef WIN32
 
