d7cb15d59a7bac9bcf41f86cd7fa19e08f690e65
xustest <520085028@qq.com>
2014-11-17 12:54:46 +0800

    some change
    Signed-off-by: xustest <520085028@qq.com>

--- framework/netio/main.cpp
+++ framework/netio/main.cpp
@@ -3,13 +3,6 @@
 
 int main(int argc, const char * argv[])
 {
-    // insert code here...
-    std::clog << sizeof(int8) << std::endl;
-    std::clog << sizeof(int16) << std::endl;
-    std::clog << sizeof(int32) << std::endl;
-    std::clog << sizeof(uint8) << std::endl;
-    std::clog << sizeof(uint16) << std::endl;
-    std::clog << sizeof(uint32) << std::endl;
     
     std::cout << "Hello, World!\n";
     return 0;
